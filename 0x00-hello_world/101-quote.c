d1fa3a36d1cefc0651f9262972561c3bbb1d9849
TasneemMahmoud <me@example.com>
2023-08-12 18:50:55 -0700

    mes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,14 +6,14 @@
  * Description: Write a C program that
  *		prints with write function
  *
- * Return: 1 (not Success)
+ * Return: 1 (error)
 */
 
 int main(void)
 {
-	char ts[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
-	write(1, ts, 59);
+	write(1, quote, 59);
 
 	return (1);
 }
