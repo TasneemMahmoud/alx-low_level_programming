32a62da2881b113c6cb922f1c2fe7a7f145c435d
TasneemMahmoud <me@example.com>
2023-08-23 15:47:27 -0700

    pointers_arrays

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,38 +1,62 @@
 #include "main.h"
+#include <stdio.h>
+
+/**
+ * islower - spacific if char is lower
+ *
+ * @ch: character
+ *
+ * Return: 1 if true, 0 if false
+ */
+
+int islower(char ch)
+{
+	return (ch >= 97 && ch <= 122);
+}
+
+/**
+ * isdelimiter - Separators of words
+ *
+ *@ch: character
+ *
+ * Return: 1 if true, 0 if false
+ */
+
+int isdelimiter(char ch)
+{
+	int i;
+	char delimiter[] = " \t\n,.!?\'(){}";
+
+	for (i = 0; i < 12; i++)
+		if (ch == delimiter[i])
+			return (1);
+}
 
 /**
  * cap_string -  function that capitalizes all words of a string.
  *
- * @str: Acapitalized string
+ * @s: Acapitalized string
  *
  * Return: Achanged string in pointer
  */
 
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
-	int i = 0;
+	char *ptr = s;
+	int founddelimiter = 1;
 
-	while (str[i])
+	while (*s)
 	{
-		while (!(str[i] >= 'a' && str[i] <= 'z'))
-			i++;
-
-		if (str[i - 1] == ' ' ||
-		(str[i - 1] == '\t' ||
-		(str[i - 1] == '\n' ||
-		(str[i - 1] == ',' ||
-		(str[i - 1] == ';' ||
-		(str[i - 1] == '.' ||
-		(str[i - 1] == '!' ||
-		(str[i - 1] == '?' ||
-		(str[i - 1] == '"' ||
-		(str[i - 1] == '(' ||
-		(str[i - 1] == ')' ||
-		(str[i - 1] == '{' ||
-		(str[i - 1] == '}' ||
-		i == 0)
-			str[i] -= 32;
-		i++;
+		if (isdelimiter(*s))
+			founddelimiter = 1;
+		else if (islower(*s) && founddelimiter)
+		{
+			*s -= 32;
+			founddelimiter = 0;
+		}
+		else
+			founddelimiter = 0;
+		s++;
 	}
-	return (str);
+	return (ptr);
 }
