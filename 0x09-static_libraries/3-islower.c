bf1f277edfca4e8a891f18aa3df4a4fb660c9678
TasneemMahmoud <me@example.com>
2023-09-01 14:02:10 -0700

    Static Librairies

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,1 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - unction that checks for lowercase character
+ *
+ * @c: checks input of function
+ *
+ * Return: return 1 if 'c' is lowercase
+ *	otherwise always 0 (success)
+ *
+*/
+
 int _islower(int c)
+{
+	if (c > 96 && c < 123)
+		return (1);
+	return (0);
+}
--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,1 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if character is a letter
+ *		both lowercase or uppercase
+ *
+ * @c: takes input from other function
+ *
+ * Return: return 1 is 'c' if true else 0
+ *
+*/
+
 int _isalpha(int c)
+{
+	if (c > 96 && c < 123)
+		return (1);
+	if (c > 64 && c < 91)
+		return (1);
+	return (0);
+}
