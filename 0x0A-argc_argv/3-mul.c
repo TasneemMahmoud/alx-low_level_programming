32f7758c0fbacc5df89a452bb1d71f58b1a4a89e
TasneemMahmoud <me@example.com>
2023-09-01 17:04:20 -0700

    argc, argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include<stdio.h>
+#include<stdlib.h>
 
 /**
  * main - program that multiplies two numbers.
@@ -16,7 +17,7 @@ int main(int argc, char **argv)
 
 	if (argc == 3)
 	{
-		res = _atoi(argv[1]) * _atoi(argv[2]);
+		res = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", res);
 	}
 	else
@@ -24,5 +25,6 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		return (1);
 	}
+
 	return (0);
 }
