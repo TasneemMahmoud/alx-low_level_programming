01ced79a48d95d264cc229bfa5e6a132f7503fec
TasneemMahmoud <me@example.com>
2023-09-11 14:37:30 -0700

    structures_typedef

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * *new_dog -  a function that creates a new dog
+ *
+ * @name: dog_t name
+ * @age:  dog_t age
+ * @owner: dog_t owner
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+
+
+
